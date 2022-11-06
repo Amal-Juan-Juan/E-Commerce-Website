1ccb28720db1a87d7149e6210681fcef45ad503b
Anna Jai Joseph <annajaijoseph97@gmail.com>
2022-11-06 15:09:30 +0530

    signup frontend done

--- signup.php
+++ signup.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
+    <link rel="stylesheet" href="signup.css">
     <link rel="stylesheet" href="Styles/index.css">
+    
 </head>
 <body>
     <?php
@@ -20,49 +22,37 @@
     <div class="Main-container">
         <div class="container-login">
             <div class="wrap-login">
-
+            <div class="signupFrm">
                 <div class="login-pic">
                     <img src="assets/logo.png" alt="IMG">
                 </div>
-
                 <form id="form" action=""class="login-form" method="POST">
-                    <span class="login-form-title">Sign Up</span>
+                <h1 class="login-form-title">Sign up</h1>
 
-                    <div class="wrap-input">
-                        <input type="text" class="input" name="email" placeholder="Email" required>
-                        <span class="focus-input"></span>
-                        <span class="symbol-input">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                    <div class="wrap-input">
-                        <input type="text" class="input" name="address" placeholder="address" required>
-                        <span class="focus-input"></span>
-                        <span class="symbol-input">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                    <div class="wrap-input">
-                        <input id="pass"type="password" class="input" name="pass" placeholder="Password" required>
-                        <span class="focus-input"></span>
-                        <span class="symbol-input">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                    <div class="wrap-input">
-                        <input id ="repass"type="password" class="input" name="repass" placeholder="Retype Password" required>
-                        <span class="focus-input"></span>
-                        <span class="symbol-input">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                        </span>
-                    </div>
+                <div class="inputContainer">
+                    <input type="text" class="input" name="email" placeholder="a" required>
+                    <label for="" class="label">Email</label>
+                </div>
 
-                    <div class="login-form-btn-container">
-                        <button class="login-form-btn">Sign Up</button>
-                    </div>
-                    <h1 id="res"><?php echo $res;?></h1>
+                <div class="inputContainer">
+                    <input type="text" class="input" name="address" placeholder="a" required>
+                    <label for="" class="label">Address</label>
+                </div>
+
+                <div class="inputContainer">
+                    <input type="password" class="input" name="pass" placeholder="a" required>
+                    <label for="" class="label">Password</label>
+                </div>
+
+                <div class="inputContainer">
+                    <input type="password" class="input" name="repass" placeholder="a" required>
+                    <label for="" class="label">Confirm Password</label>
+                </div>
+
+                <input type="submit" class="submitBtn" value="Sign up">
+                <h1 id="res"><?php echo $res;?></h1>
                 </form>
-
+            </div>
             </div>
         </div>
     </div>
